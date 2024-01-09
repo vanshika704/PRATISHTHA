45953d1274ae37d3a12c594abc446f83f7e88a38
vanshika704 <sharmavanshi704@gmail.com>
2024-01-09 22:52:38 +0530

    SOS

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,3 +1,4 @@
+import 'package:PRATISHTHA/SOS.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -80,6 +81,49 @@ class _IPCState extends State<IPC> {
         ]));
   }
 }
+class SOS extends StatefulWidget {
+  const SOS({super.key});
 
+  @override
+  State<SOS> createState() => _SOSState();
+}
 
+class _SOSState extends State<SOS> {
+  @override
+  Widget build(BuildContext context) {
+     return Container(
+      height: 80,width: 300,
+       margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 152, 214, 245),
+        ),
+        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          Text(
+            "Indian Penal Code",
+            style: TextStyle(
+              color: Color.fromARGB(255, 10, 11, 12),
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+            ),
+          ),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.read_more,
+                color: Color.fromARGB(255, 22, 72, 119),
+                size: 25,
+              )),
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const SOSbutton()));
+            },
+            child: Text("click me"),
+          )
+        ]));
+  }
+}
+ 
     
