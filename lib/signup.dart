72a06445e34610fb3f3b58bfee786599604aaa69
vanshika704 <sharmavanshi704@gmail.com>
2024-01-10 22:00:02 +0530

    logincheck

--- lib/signup.dart
+++ lib/signup.dart
@@ -2,9 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-import 'MyHomePage.dart';
 import 'login.dart';
 
 class Signup extends StatefulWidget {
@@ -19,6 +17,16 @@ class _SignupState extends State<Signup> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   var _emailController = TextEditingController();
   var _passwordController = TextEditingController();
+  final OnboardController _onboardController = Get.find<OnboardController>();
+ @override
+  void initState() {
+    super.initState();
+
+    
+    _onboardController.checkLoggedIn();
+  }
+
+  
 
   Future<void> _signUpWithEmailAndPassword() async {
     try {
@@ -57,15 +65,12 @@ class _SignupState extends State<Signup> {
 
       print("Signed in with Google: ${userCredential.user?.displayName}");
 
-     
       Get.snackbar(
         "Success",
         "Signed in with Google successfully",
         snackPosition: SnackPosition.BOTTOM,
         duration: Duration(seconds: 2),
       );
-
-    
     } catch (e) {
       print("Error signing in with Google: $e");
     }
@@ -74,7 +79,8 @@ class _SignupState extends State<Signup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor:  Color.fromARGB(255, 3, 122, 102),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 3, 122, 102),
         title: Text('SIGN UP'),
         actions: [
           IconButton(
@@ -91,8 +97,10 @@ class _SignupState extends State<Signup> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset("assets/emblem.jpg",fit: BoxFit.fill,),
-              
+              Image.asset(
+                "assets/emblem.jpg",
+                fit: BoxFit.fill,
+              ),
               TextField(
                 controller: _emailController,
                 decoration: InputDecoration(labelText: 'Email'),
@@ -104,17 +112,21 @@ class _SignupState extends State<Signup> {
                 obscureText: true,
               ),
               SizedBox(height: 16),
-              ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 3, 122, 102)) ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 3, 122, 102))),
                 onPressed: _signUpWithEmailAndPassword,
                 child: Text('Sign Up'),
               ),
               SizedBox(height: 16),
-              ElevatedButton(style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Color.fromARGB(255, 3, 122, 102)) ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                        Color.fromARGB(255, 3, 122, 102))),
                 onPressed: _signInWithGoogle,
                 child: Text('Sign In with Google'),
-                
               ),
-              
             ],
           ),
         ),
@@ -123,24 +135,10 @@ class _SignupState extends State<Signup> {
   }
 }
 
-Future<bool> checkIfLoggedIn() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? authToken = prefs.getString('authToken');
-  return authToken != null;
-}
-
-class AutoLoginCheck extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder<bool>(
-      future: checkIfLoggedIn(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return snapshot.data! ? MyHomePage() : LoginPage();
-        } else {
-          return CircularProgressIndicator();
-        }
-      },
-    );
+class OnboardController extends GetxController {
+  Future<void> checkLoggedIn() async {
+    if (!(FirebaseAuth.instance.currentUser == null)) {
+      await Get.offNamed("/MyHomePage");
+    }
   }
 }
