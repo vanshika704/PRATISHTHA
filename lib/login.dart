1ac32f62053f2b68dea81cff76866f34847d6401
vanshika704 <sharmavanshi704@gmail.com>
2024-01-19 23:23:58 +0530

    facebook

--- lib/login.dart
+++ lib/login.dart
@@ -18,6 +18,7 @@ class _LoginPageState extends State<LoginPage> {
   var _emailController = TextEditingController();
   var _passwordController = TextEditingController();
  final GoogleSignIn _googleSignIn = GoogleSignIn();
+ 
   @override
   void initState() {
     super.initState();
@@ -83,7 +84,7 @@ class _LoginPageState extends State<LoginPage> {
       print("Error signing in with Google: $e");
     }
   }
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
