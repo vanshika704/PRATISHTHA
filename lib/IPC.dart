86d391ad5228977060c431ee9ebb0eb8dc9afb6c
vanshika704 <sharmavanshi704@gmail.com>
2024-01-21 12:36:51 +0530

    ipc

--- lib/IPC.dart
+++ lib/IPC.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ipc extends StatefulWidget {
   const ipc({Key? key}) : super(key: key);
@@ -9,8 +11,30 @@ class ipc extends StatefulWidget {
 
 class _ipcState extends State<ipc> {
 
-  final List<String> items = List<String>.generate(511, (i) => 'Item $i');
+  List<String> items = List<String>.generate(511, (i) => 'Item $i');
+  @override
+   void initState() {
+    super.initState();
+   
+    fetchData();
+  }
+  Future<void> fetchData() async {
+    
+    final response = await http.get(Uri.parse('https://devgan.in/all_sections_ipc.php'));
 
+    if (response.statusCode == 200) {
+     
+      final List<dynamic> data = json.decode(response.body);
+
+      setState(() {
+       
+        items = data.map((item) => item['name'].toString()).toList();
+      });
+    } else {
+      
+      throw Exception('Failed to load data');
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
