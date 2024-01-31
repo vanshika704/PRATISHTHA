a74a70694f777b433f95e2bc6a7a7349c2846d29
vanshika704 <sharmavanshi704@gmail.com>
2024-02-01 00:04:21 +0530

    notes

--- lib/savednotes.dart
+++ lib/savednotes.dart
@@ -22,28 +22,41 @@ class _SavedState extends State<Saved> {
         ),
         backgroundColor: Color.fromARGB(221, 134, 248, 248),
       ),
-      body: FirebaseAnimatedList(
-        query: ref,
-        itemBuilder: (BuildContext context, DataSnapshot snapshot,
-            Animation<double> animation, int index) {
-       
-print('Snapshot Value: ${snapshot.value}');
+      body: Stack(
+        children: [
+         
+          Image.asset(
+            "assets/pexels-pixabay-235985.jpg",
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          ),
 
-if (snapshot.value != null) {
-  final title = snapshot.child('title').value?.toString() ?? 'No Title';
-  final content = snapshot.child('Content').value?.toString() ?? 'No Content';
+         
+          FirebaseAnimatedList(
+            query: ref,
+            itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                Animation<double> animation, int index) {
+              print('Snapshot Value: ${snapshot.value}');
 
-  print('Title: $title, Content: $content'); 
+              if (snapshot.value != null) {
+                final title =
+                    snapshot.child('title').value?.toString() ?? 'No Title';
+                final content =
+                    snapshot.child('Content').value?.toString() ?? 'No Content';
 
-  return ListTile(
-    title: Text(title),
-    subtitle: Text(content),
-  );
-} else {
-  return Container(child: Text("empty"));
-}
+                print('Title: $title, Content: $content');
 
-        },
+                return ListTile(
+                  title: Text(title),
+                  subtitle: Text(content),
+                );
+              } else {
+                return Container(child: Text("empty"));
+              }
+            },
+          ),
+        ],
       ),
     );
   }
