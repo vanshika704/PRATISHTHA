12030afaf6f29de7b6bc30e3db0ba1629d462413
vanshika704 <sharmavanshi704@gmail.com>
2024-02-04 20:28:07 +0530

    commit

--- lib/lawyer.dart
+++ lib/lawyer.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';//.
+import 'package:get/get.dart';
 
 class Lawyer extends StatefulWidget {
   Lawyer({Key? key}) : super(key: key);
