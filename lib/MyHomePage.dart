b3a19fd3065d46c0da837fa4a003e4ee21f4eab4
Vanshika Sharma <143260941+vanshika704@users.noreply.github.com>
2024-02-28 23:04:15 +0530

    Update MyHomePage.dart

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -5,7 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:get/get.dart';//.
+import 'package:get/get.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
